ab8c5f432e245d9dff66e0420f819ff1ce48ba5a
jesus2210 <jage2201@gmail.com>
2016-03-13 23:04:13 -0600

    Added products controller and migration table products

---
+++ app/Http/Controllers/products/ProductsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace facturacion\Http\Controllers\products;
+
+use Illuminate\Http\Request;
+
+use facturacion\Http\Requests;
+use facturacion\Http\Controllers\Controller;
+
+class ProductsController extends Controller
+{
+    //
+}
